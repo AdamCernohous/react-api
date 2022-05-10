b28932aa5d795b5c9705425150fc8388ed88ed0a
Adam Černohous <u19.407@pslib.cloud>
2022-05-10 10:39:49 +0200

    tailwinaxios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './main.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="max-w-[50vw] ml-auto mr-auto">
+      <h1>GET</h1>
+      <div>
+        
+      </div>
     </div>
   );
 }
